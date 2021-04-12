6f95b825ec6b9982d2fe49f498f67ce2e2ac59ba
mk <softwaremk@outlook.com>
2021-04-12 16:36:14 -0400

    climb perf charts for both 182

---
+++ perf/c182t/climb.ts
@@ -0,0 +1,17 @@
+import { NestedObject } from '../../src/util';
+
+export const climbNotes = [
+  'Table 5.8 - At 3100 lbs (Normal Climb - 90 KIAS)',
+  'Flaps Up, 2400 RPM, Full Throttle, Standard Temp, Cowl Flaps Open',
+  'Add 1.4 gals for start, taxi, takeoff allowance (*)',
+  'Mixture leaned above 3000ft for max RPM',
+  'Increase time, fuel, distance by 10% for each 10c above standard (*)',
+  '(*) applied to this calculation',
+];
+
+export const climb: NestedObject = {
+  pAlt: [0, 2000, 4000, 6000, 8000, 10000],
+  time: [0, 3, 6, 10, 14, 19],
+  fuel: [0, 0.8, 1.6, 2.5, 3.5, 4.6],
+  dist: [0, 5, 10, 16, 23, 31],
+};
